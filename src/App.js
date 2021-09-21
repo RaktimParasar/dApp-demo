623e129e3d645838a9a31dc1b8c9b3f5ead24729
RaktimParasar <raktimparasar96@gmail.com>
2021-09-21 19:04:01 +0530

    added approve & allowance methods

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Container from "@material-ui/core/Container";
 
 import { ConnectModal } from "./components/modals";
 import { Navbar } from "./components";
-import { getETHBalance, getTNodeBalance } from "./utils/contractHelpers";
+import {
+	getETHBalance,
+	getTNodeBalance,
+	approveToken,
+	getAllowance,
+} from "./utils/contractHelpers";
 
 function App() {
 	const [type, setType] = useState("");
@@ -14,9 +19,16 @@ function App() {
 	const [connecting, setConnecting] = useStateWithCallbackLazy(false);
 	const [connected, setConnected] = useState(false);
 	const [address, setAddress] = useState("");
+	const [address2, setAddress2] = useState("");
 	const [signer, setSigner] = useState(null);
 	const [ethBalance, setEthBalance] = useState("");
 	const [tNodeBalance, setTNodeBalance] = useState("");
+	const [approving, setApproving] = useState(false);
+	const [approvedAmount, setApprovedAmount] = useState("0");
+	const [spenderAddress, setSpenderAddress] = useState("");
+	const [allowance, setAllowance] = useState("");
+
+	// 0x874b6cA6143102Ebd8aAc31Be0Bd1Ac4210aA250
 
 	const open = () => {
 		setModal(true);
@@ -95,6 +107,22 @@ function App() {
 		}
 	}, [address]);
 
+	const handleChange = (e) => {
+		setAddress2(e.target.value);
+	};
+
+	const fetchAllowance = (e) => {
+		e.preventDefault();
+		getAllowance(address, address2)
+			.then((res) => {
+				console.log(res.allowance);
+				setAllowance(parseFloat(res.allowance));
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	};
+
 	useEffect(() => {
 		if (connected) {
 			fetchETHBalance();
@@ -102,11 +130,70 @@ function App() {
 		}
 	}, [connected, fetchETHBalance, fetchTNodeBalance]);
 
+	const handleSpenderAddress = (e) => {
+		setSpenderAddress(e.target.value);
+	};
+
+	const handleAmount = (e) => {
+		setApprovedAmount(e.target.value);
+	};
+
+	const handleApprove = (e) => {
+		e.preventDefault();
+		setApproving(true);
+		console.log("Hello");
+		const amount = parseFloat(approvedAmount).toFixed(10);
+		approveToken(spenderAddress, amount, signer)
+			.then(async (response) => {
+				notification["info"]({
+					key: "approval-processing-notification",
+					message: "Transaction processing",
+					description: `Your approval of ${approvedAmount} is being processed. You can view the transaction here`,
+					btn: (
+						<a
+							href={`https://kovan.etherscan.io/tx/${response.data.hash}`}
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							View on Etherscan
+						</a>
+					),
+					duration: 0,
+				});
+				await response.data.wait();
+				notification.close("approval-processing-notification");
+				notification["success"]({
+					message: "Transaction successful",
+					description: `Your approval of ${approvedAmount} is successful. You can view the transaction here`,
+					btn: (
+						<a
+							href={`https://kovan.etherscan.io/tx/${response.data.hash}`}
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							View on Etherscan
+						</a>
+					),
+					duration: 3,
+				});
+				setApproving(false);
+				setAllowance(approvedAmount);
+			})
+			.catch((err) => {
+				notification["error"]({
+					message: "Transaction error",
+					description: `Your approval of ${approvedAmount} couldn't be processed. Something went wrong. Please try again`,
+				});
+				setApproving(false);
+				console.log(err.message);
+			});
+	};
+
 	return (
 		<>
 			<Container maxWidth="sm">
 				<Navbar walletConnected={connected} type={type} onModalOpen={open} />
-				<main className="main-wallet">
+				<section className="main-wallet">
 					{connected ? (
 						<>
 							<h3 className="main-header">Account Info</h3>
@@ -127,7 +214,47 @@ function App() {
 					) : (
 						<p>Connect Wallet to get wallet information</p>
 					)}
-				</main>
+				</section>
+				{connected ? (
+					<section className="allowance">
+						<h3>Allowance Method</h3>
+						<form onSubmit={fetchAllowance}>
+							<input
+								type="text"
+								placeholder="Spender address"
+								required
+								value={address2}
+								onChange={handleChange}
+							/>
+							<button type="submit">Submit</button>
+						</form>
+						<div className="display-allowance">
+							Allowance: {allowance ? parseFloat(allowance).toFixed(2) : "0.00"}
+						</div>
+					</section>
+				) : null}
+				{connected ? (
+					<section className="approve">
+						<h3>Approve Method</h3>
+						<form onSubmit={handleApprove}>
+							<input
+								type="text"
+								placeholder="Spender address"
+								required
+								value={spenderAddress}
+								onChange={handleSpenderAddress}
+							/>
+							<input
+								type="text"
+								placeholder="Amount"
+								required
+								value={approvedAmount}
+								onChange={handleAmount}
+							/>
+							<button type="Submit">Approve</button>
+						</form>
+					</section>
+				) : null}
 			</Container>
 			<ConnectModal
 				modal={modal}
--- src/utils/contractHelpers.js
+++ src/utils/contractHelpers.js
@@ -1,9 +1,5 @@
-import {ethers} from "ethers";
-import {
-	TOKEN_ABI,
-	TOKEN_ADDRESS,
-} from "./contracts";
-
+import { ethers } from "ethers";
+import { TOKEN_ABI, TOKEN_ADDRESS } from "./contracts";
 
 const KOVAN_PROVIDER = new ethers.providers.InfuraProvider(
 	"kovan",
@@ -28,20 +24,58 @@ export const getTNodeBalance = (address) =>
 		}
 	});
 
-
 export const getETHBalance = (address) =>
-new Promise(async (resolve, reject) => {
-    try {
-        let balance = await KOVAN_PROVIDER.getBalance(address);
-        balance = ethers.utils.formatEther(balance);
-        resolve({
-            error: false,
-            balance,
-        });
-    } catch (err) {
-        reject({
-            error: true,
-            message: err.message,
-        });
-    }
-});
+	new Promise(async (resolve, reject) => {
+		try {
+			let balance = await KOVAN_PROVIDER.getBalance(address);
+			balance = ethers.utils.formatEther(balance);
+			resolve({
+				error: false,
+				balance,
+			});
+		} catch (err) {
+			reject({
+				error: true,
+				message: err.message,
+			});
+		}
+	});
+
+export const getAllowance = (address, address2) => {
+	new Promise(async (resolve, reject) => {
+		try {
+			const tokenContract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, KOVAN_PROVIDER);
+			let allowance = await tokenContract.allowance(address, address2);
+			allowance = ethers.utils.formatUnits(allowance, 18);
+			console.log(allowance);
+			resolve({
+				error: false,
+				allowance,
+			});
+		} catch (err) {
+			reject({
+				error: true,
+				message: err.message,
+			});
+		}
+	});
+};
+
+export const approveToken = (address, amount, signer) => {
+	new Promise(async (resolve, reject) => {
+		try {
+			const tokenContract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, signer);
+			const result = await tokenContract.approve(address, ethers.utils.parseUnits(amount, 18));
+
+			resolve({
+				error: false,
+				data: result,
+			});
+		} catch (err) {
+			reject({
+				error: true,
+				message: err.message,
+			});
+		}
+	});
+};
